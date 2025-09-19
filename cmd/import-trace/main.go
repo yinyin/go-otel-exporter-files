4257433654b219c08434438a65ff95a499d9a2d1
Yinyin L <yinyinl@gmail.com>
2025-09-19 10:51:05 +0800

    adjust default parameters and text messages

--- cmd/import-trace/main.go
+++ cmd/import-trace/main.go
@@ -14,8 +14,8 @@ import (
 )
 
 func parseCommandParam() (otlpEndpoint string, insecureMode bool, targetPaths []string, err error) {
-	flag.StringVar(&otlpEndpoint, "endpoint", "localhost:4317", "target OTLP gRPC endpoint")
-	flag.BoolVar(&insecureMode, "insecure", true, "allow insecure connection")
+	flag.StringVar(&otlpEndpoint, "endpoint", "[::1]:4317", "target OTLP gRPC endpoint")
+	flag.BoolVar(&insecureMode, "insecure", false, "allow clear-text connection")
 	flag.Parse()
 	if len(otlpEndpoint) == 0 {
 		err = errors.New("missing required parameter: endpoint")
@@ -52,9 +52,13 @@ func main() {
 		if fInfos, err := os.Lstat(targetPath); nil != err {
 			log.Printf("cannot read meta of target path %q: %v", targetPath, err)
 		} else if fInfos.IsDir() {
+			log.Printf("INFO: import trace folder %q ...", targetPath)
 			trace.ImportTraceFolder(ctx, client, targetPath)
 		} else if fInfos.Mode().IsRegular() {
+			log.Printf("INFO: import trace file %q ...", targetPath)
 			trace.ImportTraceFile(ctx, client, targetPath)
+		} else {
+			log.Printf("WARN: skip unsupported target path %q", targetPath)
 		}
 	}
 	log.Print("INFO: completed.")
